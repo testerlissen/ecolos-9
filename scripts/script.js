a9d7eda3213916942740d6a9d82b62bf34df0c9e
testerlissen <150612358+testerlissen@users.noreply.github.com>
2024-02-16 14:56:28 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var className = "inverted";
+var scrollTrigger = .1;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add(className);
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove(className);
+  }
+
+};
